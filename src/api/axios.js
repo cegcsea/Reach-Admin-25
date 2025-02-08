3269e9c80a606babaa8e2097c7f825d5bdee1a82
Selva Rathinam  M <selva152004@gmail.com>
2025-02-08 22:56:16 +0530

    First commit

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3001", // Adjust to your backend URL
+  baseURL: process.env.BASE_URL, // Adjust to your backend URL
 });
 
 export default instance;
